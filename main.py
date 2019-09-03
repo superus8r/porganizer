f3ab9ae7c1036a6fa4bdaab3859547bbde763bd7
Ali Kabiri <ali@kabiri.org>
2019-09-03 10:32:03 +0200

    initial commit; added the basic logic to create a file using the given suffix

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime
+
+today = datetime.date.today()
+parsedDate = today.strftime('%d.%m.%Y')
+
+# get the file suffix which comes after date in its name
+fileSuffix = raw_input("Please enter the file suffix: ") or "DailyLog"
+fileName = parsedDate + '_' + fileSuffix
+
+print("Creating File: " + fileName)
+file = open(fileName, "w+")
+print("Done!")
