be5c7507cd4c8dc2301cefd732b1833a6b69daff
Ali Kabiri <ali@kabiri.org>
2020-06-12 19:20:39 +0200

    added support for python 2

--- porganizer.py
+++ porganizer.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
+import sys
 import datetime
 
 today = datetime.date.today()
 parsedDate = today.strftime('%d.%m.%Y')
 
+message = "Please enter the file suffix: "
+defaultFileName = "DailyLog"
+
 # get the file suffix which comes after date in its name
-fileSuffix = raw_input("Please enter the file suffix: ") or "DailyLog"
+# depending on the python version.
+if sys.version_info[0] < 3:
+    fileSuffix = raw_input(message) or defaultFileName
+else:
+    fileSuffix = input(message) or defaultFileName
+
 fileName = parsedDate + '_' + fileSuffix
 
 print("Creating File: " + fileName)
